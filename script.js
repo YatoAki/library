aab519b45c3de9c55fba824a86bb47d40be90e90
Yato Aki <yatoakii@gmail.com>
2022-05-06 16:33:23 +0630

    Use function for reability

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ let addBtn = document.querySelector(".add-book");
 let addForm = document.querySelector(".input-book")
 let overlay = document.querySelector(".overlay")
 
-addBtn.addEventListener("click", () => {
+addBtn.addEventListener("click",overlayActive);
+overlay.addEventListener("click",overlayDe);
+
+
+function overlayActive(){
   addForm.classList.add("active");
   overlay.classList.add("active");
-  console.log("hi");
-});
+}
 
-overlay.addEventListener("click", ()=>{
+function overlayDe() {
   addForm.classList.remove("active");
   overlay.classList.remove("active");
-  console.log("hi");
-});
+}
